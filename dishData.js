0d3c06654d99be883fc3da4de11ec152b1a57d4a
pamx-dota <110113355+pamx-dota@users.noreply.github.com>
2022-07-28 13:48:55 +0300

    Create dishData.js

---
+++ dishData.js
@@ -0,0 +1,54 @@
+const dishData = [
+    {
+        name: "Italian pasta",
+        price: 9.55
+    },
+    {
+        name: "Rice with veggies",
+        price: 8.65
+    },
+    {
+        name: "Chicken with potatoes",
+        price: 15.55
+    },
+    {
+        name: "Vegetarian Pizza",
+        price: 6.45
+    },
+]
+const tax = 1.20;
+
+function getPrices(taxBoolean) {
+    for (obj in dishData) {
+        let dishes = dishData[obj].name
+        let prices = dishData[obj].price
+        var finalPrice;
+        if (taxBoolean === true) {
+            finalPrice = prices * tax;
+        } else if (taxBoolean === false) {
+            finalPrice = prices;
+        } else {
+            console.log("You need to pass a boolean to the getPrices call!");
+            return ('to "jump out" of the further function execution');
+        }
+        console.log(`Dish: ${dishes} Price: $${finalPrice}`);
+    };
+};
+
+function getDiscount(taxBoolean, guests) {
+    getPrices(taxBoolean);
+    if (typeof guests === 'number' && guests > 0 && guests < 30) {
+        let discount = 0;
+        if (guests < 5) {
+            discount = 5;
+        } else if (guests >= 5) {
+            discount = 10;
+        }
+        console.log('Discount is: $' + discount);
+    } else {
+        console.log('The second argument must be a number between 0 and 30');
+    }
+}
+
+getDiscount(true, 2)
+getDiscount(false, 10)
